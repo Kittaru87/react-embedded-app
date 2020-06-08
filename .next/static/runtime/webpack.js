846d32398f2ebf029c3f58f44eb0b822dfd0c6b2
Sophia Bell <sophbell87@gmail.com>
2020-06-08 11:16:59 +0100

    adding index page and annotated layout page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "55f31bbc175deeafcb0c";
+/******/ 	var hotCurrentHash = "2aa021117136d29cd926";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-React embedded app
+## React embedded app
 
+Creating a test React app for the Shopify platform using React, Node JS, Ngrok, Next.js, GraphQL and Apollo
